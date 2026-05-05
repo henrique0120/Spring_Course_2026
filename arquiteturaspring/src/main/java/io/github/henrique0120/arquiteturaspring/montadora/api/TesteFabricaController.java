fcb6b58a14caeea92549b637ff568173298a1af5
henrique0120 <henrique.sl959@hotmail.com>
2026-05-04 22:56:18 -0300

    bean qualifier

--- arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,14 +2,18 @@ package io.github.henrique0120.arquiteturaspring.montadora.api;
 
 import io.github.henrique0120.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorTurbo")
     private Motor motor;
 
     @PostMapping
