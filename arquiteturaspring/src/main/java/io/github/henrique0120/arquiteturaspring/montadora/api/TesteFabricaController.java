e40159412a0509e28714213bc88265d79689d4ec
henrique0120 <henrique.sl959@hotmail.com>
2026-05-06 21:02:14 -0300

    projeto mvc

--- arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
-    @Turbo
+    @Eletrico
     private Motor motor;
 
     @PostMapping
---
+++ arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,18 @@
+package io.github.henrique0120.arquiteturaspring.todos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService (TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        return repository.save(novoTodo);
+    }
+}
