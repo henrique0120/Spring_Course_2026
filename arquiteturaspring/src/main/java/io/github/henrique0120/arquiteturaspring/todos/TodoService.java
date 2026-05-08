e842043a30ff38d6fcaf3db9eee657ae248c5a8c
henrique.souza <henrique.sl959@hotmail.com>
2026-05-08 10:15:53 -0300

    Trabalhando com Components

--- arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/todos/TodoService.java
+++ arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/todos/TodoService.java
@@ -6,18 +6,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class TodoService {
 
-    private TodoRepository repository;
+    private final TodoRepository repository;
+    private final TodoValidator validator;
+    private final MailSender mailSender;
 
-    public TodoService (TodoRepository todoRepository){
-        this.repository = todoRepository;
+    public TodoService(TodoRepository repository, TodoValidator validator, MailSender mailSender) {
+        this.repository = repository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
 
     public TodoEntity salvar(TodoEntity novoTodo){
+        validator.validar(novoTodo);
         return repository.save(novoTodo);
     }
 
     public void atualizarStatus(TodoEntity todo){
         repository.save(todo);
+        String status = todo.getConcluido() == Boolean.TRUE ? "Concluído" : "Não concluído";
+        mailSender.enviar("Todo " + todo.getDescricao() + " foi atualizado para" + status);
     }
 
     public TodoEntity buscarPorId(Integer id){
