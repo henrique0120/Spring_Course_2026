011fde8279d9762a8db755f7ae8cda5ec0585e7b
henrique.souza <henrique.sl959@hotmail.com>
2026-05-08 09:16:05 -0300

    Demonstrando a arquitetura em funcionamento

--- arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/henrique0120/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
-    @Eletrico
+    @Turbo
     private Motor motor;
 
     @PostMapping
